3719dc39bb5e9700e10e07ebf7073de2a6813ad3
kylepadernilla <47429198+kylepadernilla@users.noreply.github.com>
2019-09-26 09:57:46 -0400

    tried combining, still didn't work

--- invert.py
+++ invert.py
@@ -5,31 +5,28 @@ with open('test.txt','r') as f:
     authorBool = False
 
     for line in f:
-        if(pubDateBool):
-            if(line.strip() == ".N"):  # Checks to see if you're finished with the publication date.
-                pubDateBool = False
-            elif(line.strip() == ".A"):  # Checks to see if you're finished with the publication date.
-                pubDateBool = False
-                authorBool = True
-            else:
-                print(line)  # returns the pubDate.
-        elif(titleBool):
-            if(line.strip() == ".B"): #Checks to see if you're finished with the title
+        if(titleBool or pubDateBool):
+            if(line.strip() == ".B" or line.strip() == "N"): #Checks to see if you're finished with the title
                 titleBool = False
+                pubDateBool = False
             elif(line.strip() == ".W"): #Checks to see if you're finished with the title
                 titleBool = False
+                pubDateBool = False
                 abstractBool = True
+
+            elif(line.strip() == ".A"):
+                titleBool = False
+                pubDateBool = False
+                authorBool = True
             else:
                 print(line) #returns the title.
 
         else:
-            if (line.strip() == ".B"):  # Looks for publication date.
-                pubDateBool = True
-
-            elif(line.strip() == ".T"): #Looks for title paragraph
+            if(line.strip() == ".T"): #Looks for title paragraph
                 titleBool = True
 
-
+            elif (line.strip() == ".B"):  # Looks for publication date.
+                pubDateBool = True
 
             list = line.split()
             for word in list:

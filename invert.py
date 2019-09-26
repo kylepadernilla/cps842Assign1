64adff960d269c877836f25d3179452e0e4c1902
kylepadernilla <47429198+kylepadernilla@users.noreply.github.com>
2019-09-26 09:51:07 -0400

    only prints last one for some reason

--- invert.py
+++ invert.py
@@ -3,33 +3,36 @@ with open('test.txt','r') as f:
     abstractBool = False
     pubDateBool = False
     authorBool = False
+
     for line in f:
         if(pubDateBool):
-            if(line == ".N"):  # Checks to see if you're finished with the publication date.
+            if(line.strip() == ".N"):  # Checks to see if you're finished with the publication date.
                 pubDateBool = False
             elif(line.strip() == ".A"):  # Checks to see if you're finished with the publication date.
                 pubDateBool = False
                 authorBool = True
             else:
                 print(line)  # returns the pubDate.
-        if(titleBool):
+        elif(titleBool):
             if(line.strip() == ".B"): #Checks to see if you're finished with the title
                 titleBool = False
             elif(line.strip() == ".W"): #Checks to see if you're finished with the title
                 titleBool = False
                 abstractBool = True
             else:
-                print(line) #returns the title
+                print(line) #returns the title.
 
         else:
-            if(line.strip() == ".T"): #Looks for title paragraph
-                titleBool = True
-
-            elif(line.strip() == ".B"): #Looks for publication date.
+            if (line.strip() == ".B"):  # Looks for publication date.
                 pubDateBool = True
 
-            list = line.split()    
-            for word in list:   
+            elif(line.strip() == ".T"): #Looks for title paragraph
+                titleBool = True
+
+
+
+            list = line.split()
+            for word in list:
                 if(word == ".I"): #Checks the split line list to see if it contains the index.
                     print(list[1]) #returns the document ID.
 

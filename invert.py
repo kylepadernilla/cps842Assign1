3f1ff5666763101ace51e38455e4a5ab35f08102
kylepadernilla <47429198+kylepadernilla@users.noreply.github.com>
2019-09-26 13:01:01 -0400

    works until there is an abstract

--- invert.py
+++ invert.py
@@ -5,28 +5,37 @@ with open('test.txt','r') as f:
     authorBool = False
 
     for line in f:
+
         if(titleBool or pubDateBool):
-            if(line.strip() == ".B" or line.strip() == "N"): #Checks to see if you're finished with the title
+            if(line.strip() == ".B"): #Checks to see if you're finished with the title
                 titleBool = False
-                pubDateBool = False
+                pubDateBool = True
             elif(line.strip() == ".W"): #Checks to see if you're finished with the title
                 titleBool = False
-                pubDateBool = False
                 abstractBool = True
-
-            elif(line.strip() == ".A"):
-                titleBool = False
+            elif (line.strip() == ".N"):  # Checks to see if you're finished with the publication date.
+                pubDateBool = False
+            elif (line.strip() == ".A"):  # Checks to see if you're finished with the publication date.
                 pubDateBool = False
                 authorBool = True
             else:
                 print(line) #returns the title.
 
-        else:
-            if(line.strip() == ".T"): #Looks for title paragraph
-                titleBool = True
+        elif(abstractBool):
+            if (line.strip() == ".B"):  # Checks to see if you're finished with the abstract
+                abstractBool = False
+            else:
+                print(line)  # returns the title.
 
-            elif (line.strip() == ".B"):  # Looks for publication date.
-                pubDateBool = True
+        elif(authorBool):
+            if (line.strip() == ".N"):  # Checks to see if you're finished with the abstract
+                authorBool = False
+            else:
+                print(line)  # returns the title.
+
+        else:
+            if (line.strip() == ".T"):  # Looks for Title.
+                titleBool = True
 
             list = line.split()
             for word in list:

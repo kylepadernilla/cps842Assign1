f7d467ae8de2ec81a703a70d7e70c779aa830151
ragesty <Harleylgdavidson@gmail.com>
2019-10-31 21:26:50 -0400

    Create eval.py

---
+++ eval.py
@@ -0,0 +1,21 @@
+f1=open('query.text','r')
+f2=open('qrels.text','r')
+
+#For the qrels comparison
+rel_query = dict()
+for line in f2:
+    pos1 = 0
+    pos2 = 0
+    position = 0
+    for segment in line.split():
+        if (position == 0):
+            pos1 = segment
+        elif (position == 1):
+            pos2 = segment
+        position += 1
+    if pos1 in rel_query:
+        rel_query[pos1].append(pos2)
+    else:
+        rel_query[pos1]= []
+        rel_query[pos1].append(pos2)
+#Finish with a dict of query:[documents] pairs
